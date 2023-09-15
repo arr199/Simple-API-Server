22b78d984cee9e5a244408383b182a850188be78
seafood12 <abieledelso@gmail.com>
2023-09-15 19:55:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import * as dotenv from "dotenv"
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT ?? 8080
-app.use(cors())
 const info = [
     {  
         username : "lola" , 
