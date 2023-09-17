02abc9efb8a07d7f83e4cbf66c2b45657b4f54bd
seafood12 <abieledelso@gmail.com>
2023-09-17 17:04:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ app.delete('/users/:id' , (req , res) => {
 app.get("/email" , (req, res) => {
     var transporter = nodemailer.createTransport({
         service: 'smtp.gmail.com',
+        secure : true , 
         port:465 , 
         auth: {
           user: 'arremailserver@gmail.com',
