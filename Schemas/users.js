1431928b3b28058cba0e137b0f6bacc59c74cc41
seafood12 <abieledelso@gmail.com>
2023-09-19 11:31:39 +0100

    Update users.js

--- Schemas/users.js
+++ Schemas/users.js
@@ -1,13 +1,13 @@
 import z  from 'zod'
 
 const userSchema = z.object({
-    user : z.string(),
+    user : z.string().maxLength(3),
     age :  z.number().int().positive().min(1).max(120 , { message : 'int numbers between 1 and 120' }) ,
-    id : z.string()
+ 
 
 })
 
 export function validateUser (object) {
-    return userSchema.safeParseAsync(object)
+    return userSchema.safeParse(object)
 
 }
